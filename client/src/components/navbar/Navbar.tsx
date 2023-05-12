60e8a586cfc70f0fcb2592b506b883e09a0b61a4
salikbubu <salikbula@gmail.com>
2023-05-12 08:29:44 +0200

    First

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row items-center justify-center'>
+      <div className='p-2'>
+        Modal - Form
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
